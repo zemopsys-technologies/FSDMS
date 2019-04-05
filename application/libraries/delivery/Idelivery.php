0065012cb597b76ab47ba4f3013ebc08c28d6cc8
nagdivya <49225647+nagdivya@users.noreply.github.com>
2019-04-05 17:50:27 +0530

    Add files via upload

--- application/libraries/delivery/Idelivery.php
+++ application/libraries/delivery/Idelivery.php
@@ -8,38 +8,38 @@ interface Idelivery
     public function edit_employee();
     public function update_employee();
     public function delete_employee();
-
+    public function deliveryhub_home();
     //Creation of deleivery_hub_admin
     //@Todo - implemented on Deliveryhub class(controller)
     //@Todo - same functionality should be there in superadmin class(controller)
     //@Todo - superadmin should accept the request from delivery hub on creating delivery hub admin
     
-    public function create_delivery_hub_admin();
-    public function insert_delivery_hub_admin();
-    public function update_delivery_hub_admin();
-    public function delete_delivery_hub_admin();
+    // public function create_delivery_hub_admin();
+    // public function insert_delivery_hub_admin();
+    // public function update_delivery_hub_admin();
+    // public function delete_delivery_hub_admin();
 
-    //Creation of deleivery_executive
-    //@Todo - implemented on Deliveryhub class(controller)
-    //@Todo - same functionality should be there in superadmin class(controller)
-    //@Todo - superadmin should accept the request from delivery hub on creating delivery executive
+    // //Creation of deleivery_executive
+    // //@Todo - implemented on Deliveryhub class(controller)
+    // //@Todo - same functionality should be there in superadmin class(controller)
+    // //@Todo - superadmin should accept the request from delivery hub on creating delivery executive
 
-    public function create_delivery_executive();
-    public function insert_delivery_executive();
-    public function update_delivery_executive();
-    public function delete_delivery_executive();
+    // public function create_delivery_executive();
+    // public function insert_delivery_executive();
+    // public function update_delivery_executive();
+    // public function delete_delivery_executive();
 
-    //@Todo - Get finished orders from kitchens.
-    public function get_order_schedule();
+    // //@Todo - Get finished orders from kitchens.
+    // public function get_order_schedule();
 
-    //@Todo - Assign finished orders from kitchens to delivery execuitive.
-    //@Todo - check bundled order is finished or not and assign bundled order to delivery executive
-    public function set_order_del_executive();
+    // //@Todo - Assign finished orders from kitchens to delivery execuitive.
+    // //@Todo - check bundled order is finished or not and assign bundled order to delivery executive
+    // public function set_order_del_executive();
 
-    //@Todo - Get addresses of assigned orders.
-    //@Todo - LOCUS-API.
-    public function get_routes();
-    public function set_routes();
+    // //@Todo - Get addresses of assigned orders.
+    // //@Todo - LOCUS-API.
+    // public function get_routes();
+    // public function set_routes();
 
 
     
